fb6af9e019d78191a1693d62187d8e570bcf1150
btee <b.kramarski@gmail.com>
2023-11-10 23:17:41 +0100

    Make to work simple example

---
+++ README.md
@@ -0,0 +1,7 @@
+# composeit
+
+`docker-compose` without docker.
+Written to have fun.
+
+There is a pretty much the same thing probably called `just-compose`
+(wanted that name ;( ), go check it out.
--- composeit/compose.py
+++ composeit/compose.py
@@ -1,4 +1,46 @@
 import yaml
+import argparse
+import pathlib
+import subprocess
+import sys
+import time
+
+
+def start(file: pathlib.Path):
+    f = yaml.safe_load(file.open())
+    for s in f["services"]:
+        print(s)
+        print(f["services"][s])
+
+    try:
+        services = [
+            subprocess.Popen(
+                #executable=s["command"],
+                args=([s["command"]] + s.get("args", [])),
+                shell=s.get("shell", False),
+                stderr=sys.stderr,
+                stdout=sys.stdout,
+            )
+            for name, s in f["services"].items()
+        ]
+        time.sleep(3)
+    finally:
+        for s in services:
+            s.terminate()
+
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(
+        prog="composeit",
+        description="Start services defined in file",
+    )
+    parser.add_argument("-f", "--file", type=pathlib.Path)
+
+    options = parser.parse_args()
+    print(options)
+
+    start(options.file)
+
+
+if __name__ == "main":
+    main()
